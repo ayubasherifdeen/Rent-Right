040ebf48a439a51729fc28756b8299c9ad657c23
ayubasherifdeen <ayubasherifdeen@gmail.com>
2026-08-29 00:34:31 +0000

    Fix superuser creation and update module path

--- start.sh
+++ start.sh
@@ -8,10 +8,10 @@ python manage.py migrate --noinput
 DJANGO_SUPERUSER_USERNAME=$DJANGO_SUPERUSER_USERNAME \
 DJANGO_SUPERUSER_EMAIL=$DJANGO_SUPERUSER_EMAIL \
 DJANGO_SUPERUSER_PASSWORD=$DJANGO_SUPERUSER_PASSWORD \
-python manage.py createsuperuser --noinput   
+python manage.py createsuperuser --noinput || true
 
 # Collect static files
 python manage.py collectstatic --noinput
 
 # Start gunicorn
-gunicorn Rent Management.wsgi --bind 0.0.0.0:${PORT:-8000}
+gunicorn rent_management.wsgi --bind 0.0.0.0:${PORT:-8000}
