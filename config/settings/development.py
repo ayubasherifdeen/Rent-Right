b09a3784b768828168627f96eb4d798e4bc67d62
Ayuba Sherif Deen <ayubasherifdeen@gmail.com>
2026-09-07 20:02:34 +0000

    cron jobs

--- config/settings/development.py
+++ config/settings/development.py
@@ -15,7 +15,7 @@ import dj_database_url
 # ─── Core ─────────────────────────────────────────────────────────────────────
 
 DEBUG = True
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0',"endurance-rhyme-flaxseed.ngrok-free.dev", "*", "https://rentrigh-gh-staging.up.railway.app/"]
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0',"endurance-rhyme-flaxseed.ngrok-free.dev", "*", "https://rentright-gh-staging.up.railway.app/"]
 
 
 # ─── Database — SQLite for local speed ───────────────────────────────────────
